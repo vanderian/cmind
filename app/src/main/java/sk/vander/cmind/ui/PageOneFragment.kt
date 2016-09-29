a09586cd0cd0c7fbb0cb73150b4075851b8807bf
Marian Vanderka <m.vanderka@thirtyseventy.digital>
2016-09-29 18:24:30 +0200

    renamed back to java because build types failed to kotlin as source dir

--- app/src/main/java/sk/vander/cmind/ui/PageOneFragment.kt
+++ app/src/main/java/sk/vander/cmind/ui/PageOneFragment.kt
@@ -1,6 +1,10 @@
 package sk.vander.cmind.ui
 
+import android.webkit.WebView
+import android.widget.ImageView
 import autodagger.AutoInjector
+import butterknife.bindView
+import com.jakewharton.rxbinding.view.clicks
 import com.squareup.picasso.Picasso
 import sk.vander.cmind.R
 import sk.vander.library.BaseFragment
@@ -16,7 +20,23 @@ import javax.inject.Inject
 class PageOneFragment : BaseFragment() {
   @Inject lateinit var picasso: Picasso
 
+  val webView by bindView<WebView>(R.id.web_view)
+  val imageView by bindView<ImageView>(R.id.image_view)
+
   override fun onInject() {
     DaggerService.getDaggerComponent<ContentActivityComponent>(context).inject(this)
+  }
+
+  override fun onResume() {
+    super.onResume()
+
+    webView.loadUrl("google.com")
+
+    subscription.add(
+        imageView.clicks().subscribe {
+          picasso.load("https://unsplash.it/200/300/?random")
+              .into(imageView)
+        }
+    )
   }
 }
